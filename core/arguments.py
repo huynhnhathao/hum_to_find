f26ff39e88da6d70e2dab98327c2f4e897be8273
huynhnhathao <18520714@gm.uit.edu.vn>
2021-12-10 22:47:37 +0700

    update args

--- core/arguments.py
+++ core/arguments.py
@@ -65,7 +65,7 @@ elif TRAIN_ON == 'kaggle':
     train_data_path = './hum_to_find/crepe_freq/train_data.pkl'
     val_data_path = './hum_to_find/crepe_freq/val_data.pkl'
     save_model_path = './'
-    pretrained_model_path = None
+    pretrained_model_path = '../input/checkpoint/model_epoch2.pt'
     
 # Trainer 
 epochs = 10000
