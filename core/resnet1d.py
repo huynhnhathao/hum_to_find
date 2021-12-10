36962a9f17f1c169a3d763d93dbe4b9d6d9966c9
huynhnhathao <18520714@gm.uit.edu.vn>
2021-12-10 20:01:59 +0700

    torchsmm

--- core/resnet1d.py
+++ core/resnet1d.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
+from torchsummary import summary
 import arguments as args
 
 
